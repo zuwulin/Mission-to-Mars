5f6b1dd885415a46b73158536a642a7293d857cd
unknown <lesprimora@gmail.com>
2022-05-13 23:11:12 -0400

    challenge added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+import Mission_to_Mars_Challenge
 
 app = Flask(__name__)
 
@@ -16,11 +16,9 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = Mission_to_Mars_Challenge.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-
-.update_one(query_parameter, {"$set": data}, options)
 
 if __name__ == "__main__":
    app.run()
